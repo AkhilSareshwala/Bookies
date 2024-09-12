7dffcfde995f8b68dedf9b2dd3f9bd5295ecbe73
AkhilSareshwala <akhilsareshwala@gmail.com>
2024-09-12 11:49:44 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import Home from './home/Home';
 import Courses from './courses/Courses';
 import Login from './Components/Login';
-import Signin from './Components/Signin'; // Import Signin if needed
+import Signin from './Components/Signin';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/course' element={<Courses />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signin' element={<Signin />} /> {/* Ensure route matches case sensitivity */}
+        <Route path='/Bookies/' element={<Home />} />
+        <Route path='/Bookies/course' element={<Courses />} />
+        <Route path='/Bookies/login' element={<Login />} />
+        <Route path='/Bookies/signin' element={<Signin />} /> {/* Ensure route matches case sensitivity */}
       </Routes>
     </BrowserRouter>
   );
